3191a8150d25112b56af73cb8cd1484cf1e7b26b
unknown <arthurvonrohrmonteiro@gmail.com>
2024-09-25 19:13:35 -0300

    azul

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function trocaCor(){
+    const root = document.querySelector(':root');
+    root.style.setProperty('--cor-um', 'blue');
+    root.style.setProperty('--cor-dois', 'blue');
+    root.style.setProperty('--cor-tres', 'blue');
+    root.style.setProperty('--cor-quatro', 'blue');
+    root.style.setProperty('--cor-cinco', 'blue');
+    root.style.setProperty('--cor-seis', 'blue');
+}
