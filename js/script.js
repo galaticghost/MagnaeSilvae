d99b36c1991ff0c6d10041d87054f3d54cc543a5
unknown <arthurvonrohrmonteiro@gmail.com>
2024-09-25 21:09:42 -0300

    gambiarra

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+let x = true;
+
 function trocaCor(){
     const root = document.querySelector(':root');
-    root.style.setProperty('--cor-um', 'blue');
-    root.style.setProperty('--cor-dois', 'blue');
-    root.style.setProperty('--cor-tres', 'blue');
-    root.style.setProperty('--cor-quatro', 'blue');
-    root.style.setProperty('--cor-cinco', 'blue');
-    root.style.setProperty('--cor-seis', 'blue');
+    if(x){
+        root.style.setProperty('--cor-um', 'blue');
+        root.style.setProperty('--cor-dois', 'blue');
+        root.style.setProperty('--cor-tres', 'blue');
+        root.style.setProperty('--cor-quatro', 'blue');
+        root.style.setProperty('--cor-cinco', 'blue');
+        root.style.setProperty('--cor-seis', 'blue');
+        x = false;
+    } else {
+        root.style.setProperty('--cor-um', '#F3E8EE');
+        root.style.setProperty('--cor-dois', '#2E2C2F');
+        root.style.setProperty('--cor-tres', '#BACDB0');
+        root.style.setProperty('--cor-quatro', '#475B63');
+        root.style.setProperty('--cor-cinco', 'black');
+        root.style.setProperty('--cor-seis', 'whitesmoke');
+        x = true;
+    }
 }
