7d2855f156fe7325679de6b6beb80ea008938960
Arthur Von Rohr Monteiro <arthurvonrohrmonteiro@gmail.com>
2024-09-26 11:05:41 -0300

    Dark mode adicionado

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@ let x = true;
 
 function trocaCor(){
     const root = document.querySelector(':root');
+    var element = document.getElementById("svg");
+    element.classList.toggle("svg-dark");
+    var element = document.getElementById("body");
+    element.classList.toggle("bodyd");
     if(x){
-        root.style.setProperty('--cor-um', 'blue');
-        root.style.setProperty('--cor-dois', 'blue');
-        root.style.setProperty('--cor-tres', 'blue');
-        root.style.setProperty('--cor-quatro', 'blue');
-        root.style.setProperty('--cor-cinco', 'blue');
-        root.style.setProperty('--cor-seis', 'blue');
+        root.style.setProperty('--cor-um', '#2E2C2F');
+        root.style.setProperty('--cor-dois', 'white');
+        root.style.setProperty('--cor-tres', '#475B63');
+        root.style.setProperty('--cor-quatro', '#ecf8f8');
+        root.style.setProperty('--cor-cinco', 'white');
+        root.style.setProperty('--cor-seis', 'black');
         x = false;
     } else {
         root.style.setProperty('--cor-um', '#F3E8EE');
